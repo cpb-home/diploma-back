fa7bc28a47103553a206f9dd53ebf01f6ab19b9f
cpb-home <cpb@bizb.ru>
2024-10-23 20:54:41 +0300

    1

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getFuck(): string {
+    return 'What the fuck!!!';
+  }
+}
